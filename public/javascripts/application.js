a4edd27a032c9c51ffb30de9d231be0cb080aac5
Jimmy Perrine <jimmy.perrine@gmail.com>
2010-06-12 23:27:09 -0400

    Added jQuery form validation plugin. All forms are currently validated on the front end as well as the backend. This will also need to be styled to look snazzy.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,4 +3,20 @@ $().ready(function() {
 	setTimeout(function() {
 		$('#flash').fadeOut('fast');
 	}, 2500);
+    $.validator.messages.required = "";
+    $('form').validate({ //uses validate plugin
+        invalidHandler: function(e, validator) {
+            var errors = validator.numberOfInvalids();
+            if (errors) {
+                var message = errors == 1
+					? 'You missed 1 field. It has been highlighted below'
+					: 'You missed ' + errors + ' fields.  They have been highlighted below';
+                $("div.error span").html(message);
+                $("div.error").show();
+            } else {
+                $("div.error").hide();
+            }
+        },
+        onkeyup: false
+    });
 });
