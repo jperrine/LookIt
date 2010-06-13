da189ceb6677f7f615306cdcb60a8cd1dc546a77
Jimmy Perrine <jimmy.perrine@gmail.com>
2010-06-13 14:48:07 -0400

    More unit tests for looks users and pages.

--- test/unit/look_test.rb
+++ test/unit/look_test.rb
@@ -1,11 +1,12 @@
 require 'test_helper'
 
 class LookTest < ActiveSupport::TestCase
-  fixtures :looks
+  fixtures :looks, :users
   
   def setup
     @valid_look = looks(:valid_look)
     @invalid_look = looks(:invalid_look)
+    @user = users(:valid_user)
   end
   
   test "invalid look attributes" do
@@ -39,5 +40,40 @@ class LookTest < ActiveSupport::TestCase
     end
     assert look_has_page
     assert page.look == @valid_look
+  end
+  
+  test "valid look has tags" do
+    assert @valid_look.tag_list.empty?
+    @valid_look.tag_list = "cool, winner, awesome"
+    assert @valid_look.save
+    assert @valid_look.tag_list == ['cool', 'winner', 'awesome']
+  end
+  
+  test "valid length constraints on title and content" do
+    title = ''
+    content = ''
+    256.times do |x|
+      title += '1'
+    end
+    501.times do |x|
+      content += '1'
+    end
+    @valid_look.title = title
+    @valid_look.content = content
+    assert !@valid_look.valid?
+    assert @valid_look.errors.invalid?(:title)
+    assert @valid_look.errors.invalid?(:content)
+    assert !@valid_look.save
+    @valid_look.title = "new title"
+    @valid_look.content = "new content"
+    assert @valid_look.save
+    assert !@valid_look.errors.invalid?(:content)
+    assert !@valid_look.errors.invalid?(:title)
+  end
+  
+  test 'look belongs to user' do
+    assert @valid_look.user_id
+    assert @valid_look.user == @user
+    assert !@user.looks.empty?
   end
 end
--- test/unit/page_test.rb
+++ test/unit/page_test.rb
@@ -1,8 +1,38 @@
 require 'test_helper'
 
 class PageTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  fixtures :pages, :looks, :users
+  
+  def setup
+    @valid_page = pages(:valid_page)
+    @invalid_page = pages(:invalid_page)
+    @look = looks(:valid_look)
+    @user = users(:valid_user)
+  end
+  
+  test "valid page" do
+    assert @valid_page.valid?
+    assert @valid_page.save
+  end
+  
+  test "invalid page properties" do
+    assert !@invalid_page.valid?
+    assert @invalid_page.errors.invalid?(:title)
+    assert @invalid_page.errors.invalid?(:content)
+    assert @invalid_page.errors.invalid?(:posted)
+    assert @invalid_page.errors.invalid?(:look_id)
+    assert !@invalid_page.save
+  end
+  
+  test "page belongs to look" do
+    assert @valid_page.look_id
+    assert @valid_page.look == @look
+    look_has_page = false
+    @look.pages.each do |p|
+      if p == @valid_page
+        look_has_page = true
+      end
+    end
+    assert look_has_page
   end
 end
