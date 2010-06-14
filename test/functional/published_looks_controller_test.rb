e2135bcbb0c2d96d317f8ee72e40bfdd001563f8
Jimmy Perrine <jimmy.perrine@gmail.com>
2010-06-14 11:42:35 -0400

    Initial sweep of functional tests are done.

--- test/functional/published_looks_controller_test.rb
+++ test/functional/published_looks_controller_test.rb
@@ -1,5 +1,47 @@
 require 'test_helper'
 
 class PublishedLooksControllerTest < ActionController::TestCase
+  fixtures :users, :looks, :pages
   
+  def setup
+    @user = users(:valid_user)
+    @look = looks(:published_look)
+    @page = pages(:published_page)
+  end
+  
+  test 'published looks index' do
+    get :index
+    assert_response :success
+    assert_template 'index'
+  end
+  
+  test 'search' do
+    get :search
+    assert_response :success
+    assert_template 'search'
+  end
+  
+  test 'search results' do
+    get :results, {:query => 'jperrine'}
+    assert_response :success
+    assert_template 'results'
+  end
+  
+  test 'view by user' do
+    get :user, {:id => @user.id}
+    assert_response :success
+    assert_template 'user'
+  end
+  
+  test 'view look directly' do
+    get :view, {:id => @look.id}
+    assert_response :success
+    assert_template 'view'
+  end
+  
+  test 'view look page' do
+    get :view, {:look_id => @look.id, :id => @page.id}
+    assert_response :success
+    assert_template 'view'
+  end
 end
