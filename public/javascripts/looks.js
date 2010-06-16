95d9113ee1cae384f9de7cd6235966e20fd5667f
Jimmy Perrine <jimmy.perrine@gmail.com>
2010-06-16 16:10:08 -0400

    Initial work on edit preview functionality, needs to be turned in to tabs and it works nicely on look creation.

--- public/javascripts/looks.js
+++ public/javascripts/looks.js
@@ -4,7 +4,23 @@ $(document).bind('click', function(e) {
         $(".pageSelect dd ul").slideUp();
 });
 $().ready(function() {
-	$('.pageSelect span').click(function() {
+	$('#PageSelector dl.pageSelect span').click(function() {
 		$('.pageSelect dd ul').slideToggle();
+	});
+	
+	$('div#editpreview a.preview').click(function() {
+		$('div#editpreview a.edit').removeClass('activeTab');
+		$(this).addClass('activeTab');
+		$('div#editcontent').hide();
+		$('div#previewcontent').empty();
+		var HTML = "<h3>"+$('#look_title').val()+"</h3><p>"+$('#look_content').val()+"</p>";
+		$('div#previewcontent').append(HTML);
+		$('div#previewcontent').show();
+	});
+	$('div#editpreview a.edit').click(function() {
+		$('div#editpreview a.preview').removeClass('activeTab');
+		$(this).addClass('activeTab');
+		$('div#previewcontent').hide();
+		$('div#editcontent').show();
 	});
 });
