ab88b52e67ee4bade84bfd6796a7b2000708dceb
Jimmy Perrine <jimmy.perrine@gmail.com>
2010-06-17 11:47:28 -0400

    Updated preview jquery to build any embedded content into the preview so now youtube videos display appropriately and it is fucking awesome.

--- public/javascripts/pages.js
+++ public/javascripts/pages.js
@@ -8,8 +8,35 @@ $(document).ready(function() {
 		$('div#previewcontent #contentDisplay').empty();
 		var HTML = "<h3>"+$('#page_title').val()+"</h3>"+$('#page_content_ifr').contents().find('body').html();
 		$('div#previewcontent #contentDisplay').append(HTML);
+		$('div#previewcontent #contentDisplay img.mceItemFlash').each(function() {
+			$(buildYoutubePlayer()).insertAfter($(this));
+			$(this).remove();
+		});
 		$('div#editcontent').slideUp('fast', function() {
 			$('div#previewcontent').slideDown('slow');
 		});
 	});
 });
+
+function getFlashURL() {
+	return $('#contentDisplay img.mceItemFlash').attr('title').split("\":\"")[1].split(',')[0];
+}
+function getHeight() {
+	return $('#contentDisplay img.mceItemFlash').attr('height');
+}
+function getWidth() {
+	return $('#contentDisplay img.mceItemFlash').attr('width');
+}
+
+function buildYoutubePlayer() {
+	var URL = getFlashURL();
+	var height = getHeight();
+	var width = getWidth();
+	var HTML = "<object width='"+width+"' height='"+height+"'>";
+	HTML += "<param name='movie' value='"+URL+"'>";
+	HTML += "</param><param name='allowFullScreen' value='true'></param>";
+	HTML += "<param name='allowscriptaccess' value='always'></param>";
+	HTML += "<embed src='"+URL+"' type='application/x-shockwave-flash' allowscriptaccess='always' allowfullscreen='true' width='"+width+"' height='"+height+"'>";
+	HTML += "</embed></object>";
+	return HTML;
+}
